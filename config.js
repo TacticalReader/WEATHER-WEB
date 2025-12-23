cf21875f866669af5aaf63f88bc3f26662c635ee
MR.NOBODY <126796695+TacticalReader@users.noreply.github.com>
2025-12-23 11:05:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ const CONFIG = {
     apiKey: "511c0d53e786d6e701870951d85c605d", 
 
     backgrounds: {
-        'Clear': 'https://youtu.be/BHACKCNDMW8?si=1-EJ0sROvAddFR3O',
-        'Clouds': 'https://youtu.be/ZJjYB-hWxMc?si=q6vH9cQa415Z5AbT',
-        'Rain': 'https://youtu.be/TLUERuTN3AU?si=KVmXdO_dk0JJDacP',
-        'Snow': 'https://youtu.be/HraWqyGTmgs?si=mA7Rh41UUnG8VRyJhttps://youtu.be/HraWqyGTmgs?si=mA7Rh41UUnG8VRyJ',
-        'Thunderstorm': 'https://youtu.be/-ZpQTmLwWtM?si=vcCDyWCiAZgvoyrR',
+        'Clear': 'https://images.unsplash.com/photo-1601297183305-6df142704ea2?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
+        'Clouds': 'https://images.unsplash.com/photo-1534088568595-a066f410bcda?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
+        'Rain': 'https://images.unsplash.com/photo-1515694346937-94d85e41e6f0?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
+        'Snow': 'https://images.unsplash.com/photo-1477601263568-180e2c6d046e?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
+        'Thunderstorm': 'https://images.unsplash.com/photo-1605727216801-e27ce1d0cc28?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
         'Drizzle': 'https://images.unsplash.com/photo-1556485689-33e55ab56127?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Mist': 'https://youtu.be/bhRmQEPvA_0?si=-rWVhXh4RSfb_hGc'
+        'Mist': 'https://images.unsplash.com/photo-1487621167305-5d248087c724?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80'
     },
     nightBackgrounds: {
         'Default': 'https://images.unsplash.com/photo-1507400492013-162706c8c05e?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Clear': 'https://youtu.be/1zQOHsos2yY?si=r2MvLVm-WszDt2G8',
+        'Clear': 'https://images.unsplash.com/photo-1532978023944-5253f3ca5d80?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
         'Clouds': 'https://images.unsplash.com/photo-1536532184021-da4272369e80?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80'
     }
 };
