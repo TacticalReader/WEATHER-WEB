8ea8e35671dc2e0803be18040fafd640a32d607c
MR.NOBODY <126796695+TacticalReader@users.noreply.github.com>
2025-12-27 21:27:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ const CONFIG = {
     apiKey: "511c0d53e786d6e701870951d85c605d", 
 
     backgrounds: {
-        'Clear': 'https://images.unsplash.com/photo-1601297183305-6df142704ea2?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Clouds': 'https://images.unsplash.com/photo-1534088568595-a066f410bcda?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Rain': 'https://images.unsplash.com/photo-1515694346937-94d85e41e6f0?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Snow': 'https://images.unsplash.com/photo-1477601263568-180e2c6d046e?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Thunderstorm': 'https://images.unsplash.com/photo-1605727216801-e27ce1d0cc28?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Drizzle': 'https://images.unsplash.com/photo-1556485689-33e55ab56127?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Mist': 'https://images.unsplash.com/photo-1487621167305-5d248087c724?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80'
+        'Clear': 'images/clear_daysky.jpg',
+        'Clouds': 'images/clouds.jpg',
+        'Rain': 'images/rain.jpg',
+        'Snow': 'images/snow.jpg',
+        'Thunderstorm': 'images/Thunder.jpg',
+        'Drizzle': 'images/Drizzle.jpg',
+        'Mist': 'images/mist.jpg'
     },
     nightBackgrounds: {
-        'Default': 'https://images.unsplash.com/photo-1507400492013-162706c8c05e?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80',
-        'Clear': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gettyimages.in%2Fvideos%2Fnight-sky-stars-background&psig=AOvVaw2TKw5mITYFNAXFqU4PiLaZ&ust=1766596653093000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIj1nMSb1JEDFQAAAAAdAAAAABAE',
-        'Clouds': 'https://images.unsplash.com/photo-1536532184021-da4272369e80?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80'
+        'Default': 'images/Nightsky-default.jpg',
+        'Clear': 'images/Night-clearsky.jpg',
+        'Clouds': 'images/night-cloudysky.jpg'
     }
 };
