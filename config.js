9915f16bda926caa9562a9db8dd91143d21df6f3
MR.NOBODY <126796695+TacticalReader@users.noreply.github.com>
2026-03-29 11:03:56 +0530

    Update background images in config.js

--- config.js
+++ config.js
@@ -1,19 +1,26 @@
 const CONFIG = {
     // Get your free API key from https://openweathermap.org/api
     apiKey: "511c0d53e786d6e701870951d85c605d", 
-
+    // Daytime Backgrounds
     backgrounds: {
-        'Clear': 'images/clear_daysky.jpg',
-        'Clouds': 'images/clouds.jpg',
-        'Rain': 'images/rain.jpg',
-        'Snow': 'images/snow.jpg',
-        'Thunderstorm': 'images/Thunder.jpg',
-        'Drizzle': 'images/Drizzle.jpg',
-        'Mist': 'images/mist.jpg'
+        "Clear": "images/clear_daysky.jpg",
+        "Clouds": "images/clouds.jpg",
+        "Rain": "images/rain.jpg",
+        "Drizzle": "images/Drizzle.jpg",
+        "Thunderstorm": "images/Thunder.jpg",
+        "Snow": "images/snow.jpg",
+        "Mist": "images/mist.jpg",
+        "Default": "images/clear_daysky.jpg"       
     },
+
     nightBackgrounds: {
-        'Default': 'images/Nightsky-default.jpg',
-        'Clear': 'images/Night-clearsky.jpg',
-        'Clouds': 'images/night-cloudysky.jpg'
+        "Default": "images/Nightsky-default.jpg",
+        "Clear": "images/Night-clearsky.jpg",
+        "Clouds": "images/night-cloudysky.jpg",
+        "Rain": "images/rainynight.webp",
+        "Drizzle": "images/DrizzleNight.webp",
+        "Thunderstorm": "images/NightLightning.webp",
+        "Snow": "images/snowynight.webp",
+        "Mist": "images/Nightsky-default.jpg"
     }
 };
