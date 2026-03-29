d23e331bf0a02aec4ce525a6a6d5be4267c7ad77
MR.NOBODY <126796695+TacticalReader@users.noreply.github.com>
2026-03-29 10:58:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,19 @@
 const CONFIG = {
+    // Get your free API key from https://openweathermap.org/api
     apiKey: "511c0d53e786d6e701870951d85c605d", 
+
     backgrounds: {
         'Clear': 'images/clear_daysky.jpg',
-        'Clear_webp': 'images/clear_day.webp',
         'Clouds': 'images/clouds.jpg',
-        'Clouds_webp': 'images/cloudy_day.webp',
         'Rain': 'images/rain.jpg',
-        'Rain_webp': 'images/rainy_day.webp',
         'Snow': 'images/snow.jpg',
-        'Snow_webp': 'images/snowy_day.webp',
         'Thunderstorm': 'images/Thunder.jpg',
-        'Thunderstorm_webp': 'images/thunderstorm.webp',
         'Drizzle': 'images/Drizzle.jpg',
-        'Drizzle_webp': 'images/drizzle.webp',
-        'Mist': 'images/mist.jpg',
-        'Mist_webp': 'images/misty_day.webp'
+        'Mist': 'images/mist.jpg'
     },
     nightBackgrounds: {
         'Default': 'images/Nightsky-default.jpg',
-        'Default_webp': 'images/night_default.webp',
         'Clear': 'images/Night-clearsky.jpg',
-        'Clear_webp': 'images/night_clear.webp',
-        'Clouds': 'images/night-cloudysky.jpg',
-        'Clouds_webp': 'images/night_cloudy.webp'
+        'Clouds': 'images/night-cloudysky.jpg'
     }
 };
